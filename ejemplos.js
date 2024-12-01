a3b81ea2f4f3081d6f79b7baaf3504e71953269a
LemanSachs29 <juanblancomoyano@gmail.com>
2024-12-01 15:20:50 +0100

    Commit

--- ejemplos.js
+++ ejemplos.js
@@ -1,22 +1,3 @@
-// 25. reverse(): Invierte el orden del array.
-console.log(array.reverse()); // [60, 40, 0, 0, 40]
-// 26. shift(): Elimina el primer elemento.
-console.log(array.shift()); // 60
-console.log(array); // [40, 0, 0, 40]
-// 27. slice(): Crea una copia parcial del array.
-console.log(array.slice(1, 3)); // [0, 0]
-// 28. some(): Verifica si algún elemento cumple una condición.
-console.log(array.some(n => n > 30)); // true
-// 29. sort(): Ordena los elementos del array.
-array.sort((a, b) => b - a); // Descendente
-console.log(array); // [40, 40, 0, 0]
-// 30. splice(): Agrega, elimina o reemplaza elementos.
-array.splice(1, 2, 100, 200); // Elimina 2 elementos desde el índice 1 y agrega 100, 200
-console.log(array); // [40, 100, 200, 0]
-// 31. [@@iterator](): Iterador de valores (implícito en bucles `for...of`).
-for (const value of array) {
-console.log(value); // 40, 100, 200, 0
-}
 // 32. toLocaleString(): Convierte los elementos a cadena con formato regional.
 console.log(array.toLocaleString()); // "40,100,200,0"
 // 33. toReversed(): Devuelve una copia del array invertida (sin modificar el original).
